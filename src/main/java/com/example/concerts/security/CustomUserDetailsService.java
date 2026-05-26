2806f4b9b1135d99913739b1e3ca1d5bd58deedb
Dimi4Real <cvetkovicdimitrije24@gmail.com>
2026-05-26 16:26:10 +0200

    JWT dependency and JWT Classes

---
+++ src/main/java/com/example/concerts/security/CustomUserDetailsService.java
@@ -0,0 +1,31 @@
+package com.example.concerts.security;
+
+import com.example.concerts.entities.User;
+import com.example.concerts.repositories.IUserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final IUserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Korisnik nije pronađen: " + email));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()))
+        );
+    }
+}
